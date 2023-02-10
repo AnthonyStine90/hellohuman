43435a90f3652701f67dc432788a406e7fdf9ff7
[Anthony] <stine.anthony@yahoo.com>
2023-02-10 15:52:41 -0500

    format

--- src/main/java/com/anthony/hellohuman/MainController.java
+++ src/main/java/com/anthony/hellohuman/MainController.java
@@ -1,7 +1,5 @@
 package com.anthony.hellohuman;
 
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("")
 
-public class MainController<Int> {
+public class MainController {
 
   @RequestMapping("")
-  public String index(@RequestParam(value = "name", required = false, defaultValue = "human") String searchQuery, @RequestParam(value = "last_name", required = false, defaultValue = "") String searchLast) {
-  
+  public String index(@RequestParam(value = "name", required = false, defaultValue = "human") String searchQuery,
+      @RequestParam(value = "last_name", required = false, defaultValue = "") String searchLast) {
+
     return "Hello " + searchQuery + " " + searchLast;
   }
 
 }
 
-// , @RequestParam(value = "times", required = false, defaultValue="") String searchMult
+// , @RequestParam(value = "times", required = false, defaultValue="") String
+// searchMult
